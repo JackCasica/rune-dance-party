436b8c5ad669e52feb71eaa4e1c2cd5c75938add
Jack Casica <Jackcasica@gmail.com>
2023-08-25 15:35:32 -0700

    Moved comment describing game state to inside hook

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,9 @@ import { Controls } from "./components/Controls.tsx";
 import { DanceFloor } from "./components/DanceFloor.tsx";
 import { Stage } from "./components/Stage";
 import { useGame } from "./hooks/useGame.ts";
+
 import type { Player } from "./types/types.ts";
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import purpleSoda from "./assets/purple-soda.mp3";
 import { playSound } from "./util/playSound.ts";
 import gameOverSound from "./assets/game-over.wav";
@@ -57,55 +58,3 @@ function App() {
 }
 
 export default App;
-
-/*
-{
-    event: {event: 'stateSync'},
-    newGame: {
-        cardStack: (10) [{…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}]
-        count: 0
-        currentPlayerIndex: 0
-        players: (4) [{…}, {…}, {…}, {…}]
-        remainingTime: 60
-        testNum: 0
-        winner: null
-        [[Prototype]]: Object
-    },
-    oldGame: {
-        cardStack: (10) [{…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}, {…}]
-        count: 0
-        currentPlayerIndex: 0
-        players: {
-            0: {
-                controls: {
-                    0: "Left Arm",
-                    1: "Right Arm",
-                    2: "Left Leg",
-                    3: "Right Leg"
-                }, 
-                correctStreak: 0, 
-             
-                key: 'ef2fd546-1713-40ac-b1e6-f4a82da79776', 
-                limbs: [1,1,1,1],
-                playerId: 'ef2fd546-1713-40ac-b1e6-f4a82da79776',
-                score: 0
-            }
-            1: {controls: Array(4), correctStreak: 0, key: '5c45a6b9-39d4-4423-b532-83093725b7e2', limbs: Array(4), …}
-            2: {controls: Array(4), correctStreak: 0, key: '3643626c-125c-4d6d-ad1e-39dc4a7bef52', limbs: Array(4), …}
-            3: {controls: Array(4), correctStreak: 0, key: '6124c10a-0d94-4e8c-9085-7802caa5a94f', limbs: Array(4), …}
-        }
-        remainingTime: 60
-        testNum: 0
-        winner: null
-        [[Prototype]]: Object
-    },
-    players: {
-        5c45a6b9-39d4-4423-b532-83093725b7e2: {avatarUrl: 'https://app.rune.ai/avatar?base=6&hair=40&tilt=1&h…16&size=420&isCropped=1&isBackgroundTransparent=0', displayName: 'Luigi', playerId: '5c45a6b9-39d4-4423-b532-83093725b7e2'}
-        6124c10a-0d94-4e8c-9085-7802caa5a94f: {avatarUrl: 'https://app.rune.ai/avatar?base=3&hair=51&tilt=0&h…17&size=420&isCropped=1&isBackgroundTransparent=0', displayName: 'Niko Bellic', playerId: '6124c10a-0d94-4e8c-9085-7802caa5a94f'}
-        3643626c-125c-4d6d-ad1e-39dc4a7bef52: {avatarUrl: 'https://app.rune.ai/avatar?base=8&hair=46&tilt=0&h…17&size=420&isCropped=1&isBackgroundTransparent=0', displayName: 'Little Sister', playerId: '3643626c-125c-4d6d-ad1e-39dc4a7bef52'}
-        ef2fd546-1713-40ac-b1e6-f4a82da79776: {avatarUrl: 'https://app.rune.ai/avatar?base=7&hair=33&tilt=0&h…15&size=420&isCropped=1&isBackgroundTransparent=0', displayName: 'Peach', playerId: 'ef2fd546-1713-40ac-b1e6-f4a82da79776'}
-        [[Prototype]]: Object
-    },
-    yourPlayerId: "0dcd44d8-2c14-4868-be87-6bf65df3d349"
-}
-*/
